577d7db93473419d0e268c9a4dccc33f95fafde5
yay <>
2022-06-27 15:41:04 +0800

    1

---
+++ src/main/java/dograre/tk/DograreApplication.java
@@ -0,0 +1,13 @@
+package dograre.tk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DograreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DograreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dograre/tk/DograreApplicationTests.java
@@ -0,0 +1,13 @@
+package dograre.tk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DograreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
